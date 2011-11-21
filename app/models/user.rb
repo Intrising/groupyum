21901dc909208963466a2c5f0bfaaf442267a699
changtimwu <changtimwu@gmail.com>
2011-11-21 15:44:39 +0800

    improve the user model to accept the auth info from omniauth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,21 @@ class User
   field :name, :type => String
   field :email, :type => String
   field :password, :type => String
-  attr_protected :provider, :uid, :name, :email
+  field :image, :type => String
+  field :fullinfo, :type => Hash
+  attr_protected :provider, :uid, :name, :email, :password, :image, :fullinfo
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.name = auth['info']['name'] || ""
+        user.email = auth['info']['email'] || ""
+        user.image = auth['info']['image'] || ""
+        user.image = auth['info']['password'] || ""
+        user.fullinfo = auth['info']
+      end
+    end
+  end
+
 end
