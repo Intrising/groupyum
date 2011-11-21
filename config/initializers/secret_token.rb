03c19393bfd07e44f59abc45a982873d150fdc5a
changtimwu <changtimwu@gmail.com>
2011-11-21 14:39:17 +0800

    initial with a typical rails 3.1 skeletal.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupyum::Application.config.secret_token = '5ad5b238655c90644638da75ab8ad24b03da9b62fb1db5b51b776f51fe2f67dad80afa34c9786ed30f75568aa37f4cc58951b58ec3b8fcb8f78cef3177bb4101'
