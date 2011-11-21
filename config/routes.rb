ba78a2e09b27b25c2c04924eea2080df8db982c2
changtimwu <changtimwu@gmail.com>
2011-11-21 16:52:15 +0800

    routes for session managements.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Groupyum::Application.routes.draw do
+  resources :users #, :only => [ :list, :show, :edit, :update ]
+  get "home/index"
+  get "home/register"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
+  match '/auth/:provider/callback' => 'sessions#create'
+  match '/auth/failure' => 'sessions#failure'
+  match '/signout' => 'sessions#destroy', :as => :signout
+  match '/signin' => 'sessions#new', :as => :signin
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -48,8 +54,7 @@ Groupyum::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  root :to => 'home#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
