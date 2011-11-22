dca04f1c73280f6fca1497b4f0c8ca8cb1310288
changtimwu <changtimwu@gmail.com>
2011-11-22 11:42:39 +0800

    * more verbose examples on default config * enable a provider only when its token/secrent was specified in Settings.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity, :fields => [:email]
-  provider :facebook, Settings.facebook_token, Settings.facebook_secret
-  provider :twitter, Settings.twitter_token, Settings.twitter_secret
-  provider :weibo, Settings.weibo_token, Settings.weibo_secret
+  provider :facebook, Settings.facebook_token, Settings.facebook_secret if  Settings['facebook_token'].present?
+  provider :twitter, Settings.twitter_token, Settings.twitter_secret if Settings['twitter_token'].present? 
+  provider :weibo, Settings.weibo_token, Settings.weibo_secret if Settings['weibo_token'].present?
 end
